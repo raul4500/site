55fffd3efcb58d1740f9bdae49a1eaa8cfaec610
raul <raul425borges@gmail.com>
2024-03-29 11:45:33 -0300

    fotos

--- login.js
+++ login.js
@@ -1,24 +1,26 @@
 document.getElementById('logar').addEventListener('click', function() {
-    var usuario = document.getElementById("usuario").value;
-    var senha = document.getElementById("senha").value;
-    if(usuario == "admin" && senha == "admin"){
-        location.href = "home.html";
-    }else{
-        alert('algo errado')
-    }
+var usuario = document.getElementById("usuario").value;
+var senha = document.getElementById("senha").value;
+if(usuario == "admin" && senha == "admin"){
+    location.href = "home.html";
+}else{
+    alert('algo errado')
+}
 })
 
 document.getElementById('cadastrar').addEventListener('click', function() {
-    alert('cadastrado com sucesso!')
-    location.href = "home.html";
+alert('cadastrado com sucesso!')
+location.href = "home.html";
 })
 
 document.getElementById('cadastre-se').addEventListener('click', function() {
 document.getElementById('sla').style.display = 'none';
+document.getElementById('fotologin').src = 'violao_login.png';
 document.getElementById('sla1').style.display = 'block';
 });
 
 document.getElementById('entrar').addEventListener('click', function() {
-    document.getElementById('sla').style.display = 'block';
-    document.getElementById('sla1').style.display = 'none';
-    });
+document.getElementById('sla').style.display = 'block';
+document.getElementById('fotologin').src = 'bateria_login.png';
+document.getElementById('sla1').style.display = 'none';
+});
