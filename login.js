7f0aca24ce770526dc3c421bd69bd8ed3b0cc486
raul <raul425borges@gmail.com>
2024-03-29 11:30:52 -0300

    first commit

---
+++ login.js
@@ -0,0 +1,24 @@
+document.getElementById('logar').addEventListener('click', function() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    if(usuario == "admin" && senha == "admin"){
+        location.href = "home.html";
+    }else{
+        alert('algo errado')
+    }
+})
+
+document.getElementById('cadastrar').addEventListener('click', function() {
+    alert('cadastrado com sucesso!')
+    location.href = "home.html";
+})
+
+document.getElementById('cadastre-se').addEventListener('click', function() {
+document.getElementById('sla').style.display = 'none';
+document.getElementById('sla1').style.display = 'block';
+});
+
+document.getElementById('entrar').addEventListener('click', function() {
+    document.getElementById('sla').style.display = 'block';
+    document.getElementById('sla1').style.display = 'none';
+    });
